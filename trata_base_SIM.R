5d26e4f1de5b5bcf9e62df9dc5503fec41abde14
Cid de Paulo Felipe dos Santos <cid.paulo@gmail.com>
2020-02-05 15:56:32 -0300

    Update
    Incluido outra expressão regular para teste.

--- trata_base_SIM.R
+++ trata_base_SIM.R
@@ -17,7 +17,7 @@ my_data <- read.delim(file, sep = ";", header = TRUE, quote="'\"")
 #retira acentuação, separa o primeiro e ultimo nome
 my_data = my_data %>% 
   mutate(NOME_TRATADO <- stri_trans_general(my_data$NOME, "Latin-ASCII"),
-         first_name <- str_extract(my_data$NOME_TRATADO, "\\w+"),
+         first_name <- str_extract(my_data$NOME_TRATADO, "\\w+"), #Outra expressão regular, testar depois: ([\w\-]+)
          last_name<-str_extract(my_data$NOME_TRATADO, '[^ ]+$'))
 
 #exporta os dados para um arquivo .txt separado por ;
